b335ba480918e1dcca2aa7cde5b19e098559f4ed
jnelson10 <73327623+jnelson10@users.noreply.github.com>
2020-11-10 15:41:09 -1000

    Create footer.php
    footer file for all the pages in the UH Secuirty website

---
+++ footer.php
@@ -0,0 +1,93 @@
+              </div>
+            </div>
+
+          
+        
+        
+      </div><!-- #container -->
+    
+
+</div></main>
+</body>
+
+<footer>
+   <div id="footer_top">
+      <div id="footer_top">
+         <div class="container pt-4 pb-5" id="footer_top_content">
+            <div class="row">
+               <div class="footer-top-left-column contact-info col-md-8">
+                  <h2>Information Technology Services</h2>
+                  <address>
+                                             Information Technology Center                                             <br />2520 Correa Road                                             <br />Honolulu, Hawaii 96822                  </address>
+                                       <div class="contact">
+                        <strong>Contact Us</strong>
+                                                   <div class="telephone">Telephone: (808) 956-8883</div>
+                                                                                                   <div class="fax">Email: help@hawaii.edu</div>
+                                             </div>
+                                                                                                                                             </div>
+               <div class="footer-top-middle-column col-md-4">
+                                 </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div id="footer_btm">
+      <div id="footer_btm_content" class="container">
+         <div class="row">
+            <div class="uh_col col-md-3">
+               <img class="pb-3" src="https://www.hawaii.edu/its/wp-content/themes/system2018/images/footer-logo.png" srcset="https://www.hawaii.edu/its/wp-content/themes/system2018/images/footer-logo.png 1x, https://www.hawaii.edu/its/wp-content/themes/system2018/images/footer-logo-2x.png 2x" alt="uh system logo" /><br />2444 Dole Street<br />Honolulu, HI 96822
+            </div>
+            <div class="uh_col col-md-3">
+               An <a href="https://www.hawaii.edu/offices/eeo/policies/">equal opportunity/affirmative action institution</a><br />
+               Use of this site implies consent with our <a href="https://www.hawaii.edu/infotech/policies/itpolicy.html">Usage Policy</a><br />
+               copyright &copy; 2018 <a href="https://www.hawaii.edu/">University of Hawai&#699;i</a>
+            </div>
+            <div class="uh_col col-md-3">
+               <ul>
+                  <li><a href="https://www.hawaii.edu/calendar/">Calendar</a></li>
+                  <li><a href="https://www.hawaii.edu/directory/">Directory</a></li>
+                  <li><a href="https://www.hawaii.edu/emergency/">Emergency Information</a></li>
+                  <li><a href="https://myuh.hawaii.edu/">MyUH</a></li>
+                  <li><a href="http://workatuh.hawaii.edu/">Work at UH</a></li>
+               </ul>
+            </div>
+            <div class="uh_col col-md-3">
+               <div id="footer_smrow"><a href="https://twitter.com/UHawaiiNews"><img src="https://www.hawaii.edu/its/wp-content/themes/system2018/images/icon-twitter.png" alt="twitter" class="footer_smicon" /></a> &nbsp; <a href="https://www.facebook.com/universityofhawaii"><img src="https://www.hawaii.edu/its/wp-content/themes/system2018/images/icon-facebook.png" alt="facebook" class="footer_smicon" /></a> &nbsp; <a href="https://instagram.com/uhawaiinews/"><img src="https://www.hawaii.edu/its/wp-content/themes/system2018/images/icon-instagram.png" alt="instagram" class="footer_smicon" /></a> &nbsp; <a href="http://www.flickr.com/photos/uhawaii"><img src="https://www.hawaii.edu/its/wp-content/themes/system2018/images/icon-flickr.png" alt="flickr" class="footer_smicon" /></a> &nbsp; <a href="https://www.youtube.com/user/uhmagazine"><img src="https://www.hawaii.edu/its/wp-content/themes/system2018/images/icon-youtube.png" alt="youtube" class="footer_smicon" /></a></div>
+
+               <p><a href="https://www.hawaii.edu/contact/">Contact UH</a><br />
+               If required, information contained on this website can be made available in an alternative format upon request.</p>
+               <p><a href="https://get.adobe.com/reader/">Get Adobe Acrobat Reader</a></p>
+            </div>
+         </div>
+      </div>
+   </div>
+   <a href="#top" class="go-top">
+      <span class="fa fa-chevron-up" aria-hidden="true"><span class="screen-reader-text">Go to Top</span></span>
+   </a>
+   <!-- Global site tag (gtag.js) - Google Analytics -->
+   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121177752-1"></script>
+   <script>
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+
+     gtag('config', 'UA-121177752-1');
+   </script>
+</footer>
+
+<script type='text/javascript' src='https://www.hawaii.edu/its/wp-includes/js/wp-embed.min.js?ver=5.5.3' id='wp-embed-js'></script>
+<!-- site improve -->
+<script type="text/javascript">
+/*<![CDATA[*/
+(function() {
+var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+sz.src = '//siteimproveanalytics.com/js/siteanalyze_6000634.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+})();
+/*]]>*/
+</script>
+
+
+</body>
+
+</html>
